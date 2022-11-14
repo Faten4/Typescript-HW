86bbed9786a4f5f59147a47f4110a1e4431259db
faten <faten.x44@gmail.com>
2022-11-14 23:33:17 +0300

    add file

---
+++ index.js
@@ -0,0 +1,8 @@
+// let username:String ="Faten"
+var user1 = {
+    name: 'Faten',
+    Age: 25,
+    Specialization: 'Student',
+    bestfriends: '1-Amal 2-Fai 3-Jehan'
+};
+console.log(user1);
---
+++ index.ts
@@ -0,0 +1,32 @@
+// let username:String ="Faten"
+
+// username="Alruwaili";
+
+// console.log(username);
+
+// let sum:number 
+// const add =(num1:number, num2:number) => {
+//     return num1+num2;
+//     console.log(add);
+    
+// }
+// const sub =(num1:number, num2:number) => {
+//     return num1+num2;
+//     console.log(sub);
+    
+// }
+interface USERID{
+    name:string;
+     Age: number;
+     Specialization:string;
+     bestfriends: string;
+ 
+ }
+ const user1:USERID={
+     name:'Faten',
+      Age: 25,
+      Specialization:'Student',
+      bestfriends: '1-Amal 2-Fai 3-Jehan',
+  
+  }
+  console.log(user1);
